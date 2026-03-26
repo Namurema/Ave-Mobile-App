8df83dd39d8e8ed74667364378cab5af02e1ff83
Brendah <namuremabrendah@gmail.com>
2026-03-26 03:11:27 +0300

    native wind fix

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,8 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
-
-config.resolver.sourceExts.push("css");
 
 module.exports = withNativeWind(config, { 
   input: "./global.css",
-  
 });
